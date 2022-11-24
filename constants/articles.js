ebb1b3b4e10a7356ab4d4c9d4fc7295a27a62467
David Gurrola <davidgurr008@gmail.com>
2022-11-24 17:16:41 -0600

    0.2.0 componentes menú & rutas

---
+++ constants/articles.js
@@ -0,0 +1,30 @@
+import { navidad } from '../assets/imgs/navidad.png';
+export default [
+  {
+    title: 'La subasta en la palma de tus manos',
+    image: 'https://marketing4ecommerce.co/wp-content/uploads/2020/04/Subasta-Online-compressor.jpg',
+    cta: 'Ver Artículo', 
+    horizontal: true
+  },
+  {
+    title: 'Is makeup one of your daily esse …',
+    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    cta: 'Ver Artículo '
+  },
+  {
+    title: 'Coffee is more than just a drink: It’s …',
+    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+    cta: 'Ver Artículo ' 
+  },
+  {
+    title: 'La moda está de regreso, porque no hacer oferta por tu ropa favorita',
+    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+    cta: 'Ver Artículo ' 
+  },
+  {
+    title: 'Celebra la navidad con increibles subasta en ShoppingFan',
+    image: 'https://www.24-horas.mx/wp-content/uploads/2020/11/Disen%CC%83o-sin-ti%CC%81tulo-9-1.png?w=800',
+    cta: 'Ver Artículo ', 
+    horizontal: true
+  },
+];
